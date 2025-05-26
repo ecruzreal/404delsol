7b747336df50869895202fbab5901a4dd682fb47
ecruzreal <ecruzrea@uci.edu>
2025-05-25 22:06:09 -0700

    removed links to application page, information that the applications for show 6 are now closed.

--- src/Apply.jsx
+++ src/Apply.jsx
@@ -1,10 +1,16 @@
 import Contact from "./Contact";
 import ContactForm from "./ContactForm";
+import logo from '/src/404logo.png';
 
 function Apply(){
     return (
         <div className="apply-body">
-            <ContactForm />
+            <div className="apps-closed">
+                <img src={logo} alt="" />
+                <p>Applications for Show 6 have now closed! If you have any questions, send us an email at
+                    404delsol@gmail.com, or DM us on <a href="https://www.instagram.com/404delsol.uci/">instagram</a>
+                </p>
+            </div>
         </div>
     )
 };
