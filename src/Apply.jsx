fee410413640aa85580396078a9e9b43bbf592de
ecruzreal <ecruzrea@uci.edu>
2025-10-06 13:48:56 -0700

    closed applications for show 7

--- src/Apply.jsx
+++ src/Apply.jsx
@@ -1,11 +1,12 @@
 import Contact from "./Contact";
 import ContactForm from "./ContactForm";
 import logo from '/src/404logo.png';
+import './Apply.css'
 
 function Apply(){
     return (
         <div className="apply-body">
-            <ContactForm></ContactForm>
+            <h1 className="closed-message">APPLICATIONS HAVE NOW CLOSED, WE'LL SEE YOU SOON!</h1>
         </div>
     )
 };
