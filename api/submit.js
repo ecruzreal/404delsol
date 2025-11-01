736d85419564769c12d7108ce6e78f36ac68eaaa
ecruzreal <ecruzrea@uci.edu>
2025-10-31 19:15:15 -0700

    debuggin test 1

--- api/submit.js
+++ api/submit.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      host: "smtp.gmail.com",
-      secure: true,
+      //host: "smtp.gmail.com",
+      //secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
