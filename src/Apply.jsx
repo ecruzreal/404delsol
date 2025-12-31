0557eb13e73c12fd6a68c5103adb84f0dabc1de4
ecruzreal <ecruzrea@uci.edu>
2025-12-30 18:20:28 -0800

    Preliminary show 9 update

--- src/Apply.jsx
+++ src/Apply.jsx
@@ -6,9 +6,11 @@ import './Apply.css'
 function Apply(){
     return (
         <div className="apply-body">
-            <p className="closed-message">APPLICATIONS ARE CLOSED, WE'LL SEE YOU SOON</p>
+            <ContactForm></ContactForm>
         </div>
     )
 };
+
+//<p className="closed-message">APPLICATIONS ARE CLOSED, WE'LL SEE YOU SOON</p>
 
 export default Apply;
