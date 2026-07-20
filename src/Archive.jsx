21298738ad0882f67eb60debf595235f2e97ea8a
ecruzreal <ecruzrea@uci.edu>
2026-07-19 17:01:25 -0700

    Add show 10 images

--- src/Archive.jsx
+++ src/Archive.jsx
@@ -56,6 +56,12 @@ const mediasets = {
     ],
     "show-9":[
         "show9_1.png", "show9_2.png"
+    ],
+    "show-10":[
+        "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg",
+        "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg",
+        "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg",
+        "26.jpg", "27.jpg"
     ]
 };
 
@@ -107,6 +113,9 @@ function Archive() {
                     </option>
                     <option value="/archive/show-9">
                         SHOW 9
+                    </option>
+                    <option value="/archive/show-10">
+                        SHOW 10
                     </option>
                 </select>
                 <div className="archive-main">
